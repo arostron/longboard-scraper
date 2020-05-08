57bfadaa87598c4ab3de36cac19cba6c65f1e895
arostron <alex.rostron24@gmail.com>
2020-05-07 20:12:40 -0600

    add long data regex and urls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use requests::*;
+
 fn main() {
-    println!("Hello, world!");
+	let boards = vec!["dinghy-blunt-hyena", "dinghy-crown-peak",  "dinghy-turbo-king", "dinghy-turbo", "tugboat-wolf"];
+	let base_url = "https://landyachtz.com/ca/product/";
+	let _i_hate_regex = "(\\|?\\s?[A-Z]+:\\s?)(\\s[0-9]+[\\.]?[0-9]?\"\\s?)(\\|?\\s?[A-Z]+:\\s?)(\\s[0-9]+[\\.]?[0-9]?\"\\s?)(\\|?\\s?[A-Z]+:\\s?)(\\s[0-9]+[\\.]?[0-9]?\"\\s?)";
+
+	let board = boards[0];
+	get(format!("{}{}", base_url, board)).unwrap();
 }
